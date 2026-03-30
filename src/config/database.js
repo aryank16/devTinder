3e6bc5ad8ba96bf5b262fab894ff5c6525c10ec1
ARYAN KOODI <127104749+aryank16@users.noreply.github.com>
2026-03-30 23:56:46 +0200

    changes

--- src/config/database.js
+++ src/config/database.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
 
     await mongoose.connect(
-    "mongodb+srv://aryankood9_db_user:2MJMoj0phalGfTe1@nanastenode.qshwu6a.mongodb.net/devTinder"
+    process.env.DB_CONNECTION_SECRET
 )
 
 } 
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -13,7 +13,7 @@
             return res.status(401).send('Please login')
         }
 
-        const decodedObj = await jwt.verify(token, 'DEV@Tinder$790');
+        const decodedObj = await jwt.verify(token, process.env.JWT_SECRET);
         const { _id } = decodedObj;
 
         const user = await User.findById(_id);
